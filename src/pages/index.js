fa325f859bd67bf88e524e4103c3dcbc91fe1b39
maribelbhf <maribelbhf@gmail.com>
2020-07-05 15:03:27 -0400

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Header from "../components/header"
 import Layout from "../components/layout"
-import 'font-awesome/css/font-awesome.min.css';
 import SEO from "../components/seo"
 
 const IndexPage = () => (
